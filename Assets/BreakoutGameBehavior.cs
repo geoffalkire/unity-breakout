93bb315c69e48446ceb5723373911d67d129402a
Geoff Alkire <geoff.alkire@gmail.com>
2014-03-19 12:17:42 -0500

    Added brick random color selection logic and brick destruction on collision. Setup brick, ball, and paddle as prefabs.

---
+++ Assets/BreakoutGameBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakoutGameBehavior : MonoBehaviour {
+	public Transform BrickPrefab;
+	public Vector2 BrickSetOffset;
+	public float BrickWidth;
+	// Use this for initialization
+	void Start () {
+		InitializeBricks();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void InitializeBricks()
+	{
+		//create a set of 10 bricks across the top
+		for (var i = 0; i < 10; i++) {
+			var brickInstance = (Transform)Instantiate (BrickPrefab, new Vector3(i * BrickWidth * BrickPrefab.localScale.x + BrickSetOffset.x, BrickSetOffset.y, 0), Quaternion.identity);
+			brickInstance.GetComponent<BrickBehavior>().SetRandomColor();
+				}
+
+	}
+}
---
+++ Assets/BrickBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BrickBehavior : MonoBehaviour {
+
+	public List<Sprite> AvailableSprites;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void SetRandomColor()
+	{
+		int randomIndex = Random.Range (0, AvailableSprites.Count - 1);
+		//Sprite sprite = (Sprite)Resources.Load ("sprites_7", typeof(Sprite));
+		GetComponent<SpriteRenderer>().sprite = AvailableSprites[randomIndex];
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		//only care about ball collisions
+		if (collision.collider.tag != "Ball") return;
+
+		Destroy (gameObject);
+		//var newXVelocity = calculateReboundValue(collision.transform.position.x, collision.rigidbody.velocity.magnitude);
+		//Debug.Log ("Paddle Loc: " + transform.position + " Ball Loc: " + collision.transform.position);
+		//Debug.Log ("New X Velocity: " + newXVelocity);
+		//collision.collider.rigidbody2D.velocity = new Vector2(newXVelocity, collision.collider.rigidbody2D.velocity.y);
+		//collision.collider.rigidbody2D.velocity = CalculateReboundVelocityVector(collision.transform.position, collision.rigidbody.velocity.magnitude);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
