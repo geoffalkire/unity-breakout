2808cc86a4f9d72b60af84cc3d69325aa3cb2723
Geoff Alkire <geoff.alkire@gmail.com>
2014-03-19 11:00:27 -0500

    Got rebound angle working. Updated gitignore with some additional file types

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -6,6 +6,7 @@ public class PlayerControls : MonoBehaviour {
 	public KeyCode MoveLeft;
 	public KeyCode MoveRight;
 	public float Speed = 10;
+	public float MaxAngle = 75;
 	// Use this for initialization
 	void Start () {
 	
@@ -20,31 +21,28 @@ public class PlayerControls : MonoBehaviour {
 	//called when item is collided with
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.collider.tag == "Ball") {
-			var newXVelocity = calculateReboundValue(collision.transform.position.x);
-			Debug.Log ("Looking at collider " + collision.collider.tag);
-			collision.collider.rigidbody2D.velocity = new Vector2(newXVelocity, collision.collider.rigidbody2D.velocity.y);
-		}
+        //only care about ball collisions
+	    if (collision.collider.tag != "Ball") return;
+
+	    //var newXVelocity = calculateReboundValue(collision.transform.position.x, collision.rigidbody.velocity.magnitude);
+	    Debug.Log ("Paddle Loc: " + transform.position + " Ball Loc: " + collision.transform.position);
+		//Debug.Log ("New X Velocity: " + newXVelocity);
+	    //collision.collider.rigidbody2D.velocity = new Vector2(newXVelocity, collision.collider.rigidbody2D.velocity.y);
+		collision.collider.rigidbody2D.velocity = CalculateReboundVelocityVector(collision.transform.position, collision.rigidbody.velocity.magnitude);
 	}
 
-	private float calculateReboundValue(float locationX)
+	private Vector2 CalculateReboundVelocityVector(Vector2 ballPosition, float ballSpeed)
 	{
 		//Debug.Log ("collider2d = " + collider2D);
 		BoxCollider2D collider = (BoxCollider2D)collider2D;
 
-		var leftSide = transform.position.x - (collider.size.x / 2);
-		var rightSide = transform.position.x + (collider.size.x / 2);
-		float topRatio = Mathf.Abs (locationX - leftSide);
-		float bottomRatio = Mathf.Abs (rightSide - leftSide);
-		float ratio = Mathf.Abs (transform.position.x - leftSide) / Mathf.Abs (rightSide - leftSide);
-		Debug.Log ("Calculated ratio is " + topRatio + "/" + bottomRatio);
+		var relativeXIntersect = ballPosition.x - (transform.position.x + (collider.size.x / 2f));
+		var normalizedRelativeXIntersect = relativeXIntersect / (collider.size.x / 2f);
 
-		float range = 5f;
-		float increment = 1f;
-		//float bottomValue = -7.5f;
+		var bounceAngle = normalizedRelativeXIntersect * (Mathf.Deg2Rad * MaxAngle);
 
-		//return value between -5 and 5
-		return (ratio * range) - (range/2);
+		return new Vector2 (ballSpeed * Mathf.Sin (bounceAngle), ballSpeed * Mathf.Cos (bounceAngle));
+
 	}
 
 }
