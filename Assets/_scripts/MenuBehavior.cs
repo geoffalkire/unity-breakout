e857e88291fd52f29525af42dc3fcf8056e09786
Geoff Alkire <geoff.alkire@gmail.com>
2014-03-21 15:44:01 -0500

    Rearranged some assets into appropriate folders. Added the title screen with a play button that loads up the "game" scene.

---
+++ Assets/_scripts/MenuBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width/2f - 40f, Screen.height/2f - 10f, 80, 20), "Play")) 
+		{
+			Application.LoadLevel ("breakoutGame");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
